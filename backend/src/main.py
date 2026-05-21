c3d6316c76aa9ad2977cf5e20b420124cbb1594f
Manokowsik <marimanokowsik2004@gmail.com>
2026-05-21 19:43:39 +0530

    Fix CORS for correct Vercel URL

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://task-manager-wllb.vercel.app"
+        "https://task-manager-wllb-manokowsik-task-management.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
