e24f9768c6166d34b6e8876bbbcf9b96a3d38597
Manokowsik <marimanokowsik2004@gmail.com>
2026-05-21 18:41:52 +0530

    updated render api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
+const API = axios.create ({
+    baseURL: "https://task-manager-api-vbca.onrender.com"
+})
 
 export default API;
